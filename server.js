212e4c7f35b2baec0a85a8bf4ea55f6a178b0fce
maneast <maneast@ahmad>
2025-02-07 03:06:40 +0800

    handle-response

--- server.js
+++ server.js
@@ -1,28 +1,66 @@
+// 100-199 : informational responses.
+// 200-299 : successful responses.
+// 300-399 : redirect.
+// 400-499 : client error.
+// 500-599 : server errors.
+
 const http = require('http');
 
 console.log('Hello, kita akan belajar membuat server');
 
 const requestListener = ( request, response ) => {
-    response.setHeader('Content-Type', 'text/html');
-    response.statusCode = 200;
+    response.setHeader('Content-Type', 'application/json');
+    response.setHeader('Powered-By', 'Node.js');
 
-    const { method } = request;
-    if (method === 'GET'){
-        response.end('<h1>Hello!</h1>');
-    }
+    const { method, url } = request;
+    if ( url === '/' ) {
 
-    if (method === 'POST'){
-        let body = [];
+        if ( method === 'GET' ){
+            response.statusCode = 200;
+            response.end(JSON.stringify({
+                message: 'Ini adalah homepage',
+            }));
+        } else {
+            response.statusCode = 400;
+            response.end(JSON.stringify({
+                message:  `Halaman tidak dapat diakses dengan ${method} request`,
+            }));
+        }
 
-        request.on('data', (chunk) => {
-            body.push(chunk);
-        });
+    } else if ( url === '/about' ) {
+
+        if ( method === 'GET' ){
+            response.statusCode = 200;
+            response.end(JSON.stringify({
+                message: 'Hello! Ini adalah halaman about',
+            }));
+        } else if ( method === 'POST' ){
+            let body = [];
     
-        request.on('end', () => {
-            body = Buffer.concat(body).toString();
-            const { name } = JSON.parse(body);
-            response.end(`<h1>Hai, ${name}!</h1>`);
-        })
+            request.on( 'data', ( chunk ) => {
+                body.push( chunk );
+            });
+        
+            request.on( 'end', () => {
+                body = Buffer.concat( body) .toString();
+                const { name } = JSON.parse( body );
+                response.statusCode = 200;
+                response.end(JSON.stringify({
+                    message: `Halo, ${name}! Ini adalah halaman about`,
+                }));
+            })
+        } else {
+            response.statusCode = 400;
+            response.end(JSON.stringify({
+                message:  `Halaman tidak dapat diakses dengan ${method} request`,
+            }));
+        }
+
+    } else {
+        response.statusCode = 404;
+        response.end(JSON.stringify({  
+            message: 'Halaman tidak ditemukam!',
+        }));
     }
 };
 
