dda795d8031de1a440ce48cdd69009518b1b2e97
maneast <maneast@ahmad>
2025-02-07 00:43:44 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const http = require('http');
+
+console.log('Hello, kita akan belajar membuat server');
+
+const requestListener = ( request, response ) => {
+    response.setHeader('Content-Type', 'text/html');
+
+    response.statusCode = 200;
+    response.end('<h1>Hello HTTP Server!</h1>');
+};
+
+
+const server = http.createServer(requestListener);
+
+const port = 5000;
+const host = 'localhost';
+
+
+server.listen(port, host, () => {
+    console.log(`Server berjalan pada http://${host}:${port}`);
+});
