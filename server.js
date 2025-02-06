71a71056b5ba32cffb68994a4227ccc2418572ef
maneast <maneast@ahmad>
2025-02-07 01:18:55 +0800

    handle-request

--- server.js
+++ server.js
@@ -4,9 +4,26 @@ console.log('Hello, kita akan belajar membuat server');
 
 const requestListener = ( request, response ) => {
     response.setHeader('Content-Type', 'text/html');
-
     response.statusCode = 200;
-    response.end('<h1>Hello HTTP Server!</h1>');
+
+    const { method } = request;
+    if (method === 'GET'){
+        response.end('<h1>Hello!</h1>');
+    }
+
+    if (method === 'POST'){
+        let body = [];
+
+        request.on('data', (chunk) => {
+            body.push(chunk);
+        });
+    
+        request.on('end', () => {
+            body = Buffer.concat(body).toString();
+            const { name } = JSON.parse(body);
+            response.end(`<h1>Hai, ${name}!</h1>`);
+        })
+    }
 };
 
 
